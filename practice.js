deb2666c91faefc610db09cc6de8c1874beff0aa
Nasir-Hussain786 <starbro646@gmail.com>
2025-12-30 12:35:43 +0500

    js data types practice

---
+++ practice.js
@@ -0,0 +1,37 @@
+let firstname = "Nasir";
+    lastname = 'Hussain';
+
+let cnic = 4530100000003;
+
+let a = true;
+    b = false;
+
+let fullname = "Nasir " + "Hussain " + "khanzada";
+
+
+document.write(firstname);
+document.write("<br><br>");
+document.write(typeof firstname);
+
+document.write("<br><br>");
+document.write(lastname);
+document.write("<br><br>");
+document.write(typeof lastname);
+
+document.write("<br><br>");
+document.write(cnic);
+document.write("<br><br>");
+document.write(typeof cnic);
+
+document.write("<br><br>");
+document.write(a);
+document.write("<br><br>");
+document.write(typeof a);
+
+document.write("<br><br>");
+document.write(b);
+document.write("<br><br>");
+document.write(typeof b);
+
+document.write("<br><br>");
+document.write(fullname);
