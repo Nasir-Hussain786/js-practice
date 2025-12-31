9f5deea79b9e0fb7d630eef36c344dc50d0dc286
Nasir-Hussain786 <starbro646@gmail.com>
2025-12-31 22:03:52 +0500

    comment all code

--- practice.js
+++ practice.js
@@ -1,67 +1,67 @@
-// ------ string variables ---------
-let firstname = "Nasir";
-let lastname = 'Hussain';
-let caste = "khanzada Rajput";
-let counrty = "Pakistan"
-let province = 'Sindh';
-let city = 'Tharushah';
+// // ------ string variables ---------
+// let firstname = "Nasir";
+// let lastname = 'Hussain';
+// let caste = "khanzada Rajput";
+// let counrty = "Pakistan"
+// let province = 'Sindh';
+// let city = 'Tharushah';
 
-// ------ number variables ---------
-let cnic = 4530100000003;
-let phoneNumber = 9230000000090;
-let pincode = 75884;
-let score = 32232;
-let zipcode = 32222;
+// // ------ number variables ---------
+// let cnic = 4530100000003;
+// let phoneNumber = 9230000000090;
+// let pincode = 75884;
+// let score = 32232;
+// let zipcode = 32222;
 
-// ------ boolean variables --------
-let a = true;
-let b = false;
-let c = true;
-let d = true;
-let e = false;
-
-
-// ------ concatenation --------
-let fullname = "Nasir " + "Hussain " + "khanzada";
-console.log(fullname);
-
-let firstname1 = "Arshad";
-let lastname1 = "Ali"
-let fullname1 = firstname1 + " " + lastname1;
-console.log(fullname1);
-
-let brand = "Apple";
-let product = "iPhone";
-let item = brand + " " + product;
-console.log(item);
+// // ------ boolean variables --------
+// let a = true;
+// let b = false;
+// let c = true;
+// let d = true;
+// let e = false;
 
 
+// // ------ concatenation --------
+// let fullname = "Nasir " + "Hussain " + "khanzada";
+// console.log(fullname);
+
+// let firstname1 = "Arshad";
+// let lastname1 = "Ali"
+// let fullname1 = firstname1 + " " + lastname1;
+// console.log(fullname1);
+
+// let brand = "Apple";
+// let product = "iPhone";
+// let item = brand + " " + product;
+// console.log(item);
 
 
-// ---------- display all values and types ----------
-document.write(firstname);
-document.write("<br><br>");
-document.write(typeof firstname);
 
-document.write("<br><br>");
-document.write(lastname);
-document.write("<br><br>");
-document.write(typeof lastname);
 
-document.write("<br><br>");
-document.write(cnic);
-document.write("<br><br>");
-document.write(typeof cnic);
+// // ---------- display all values and types ----------
+// document.write(firstname);
+// document.write("<br><br>");
+// document.write(typeof firstname);
 
-document.write("<br><br>");
-document.write(a);
-document.write("<br><br>");
-document.write(typeof a);
+// document.write("<br><br>");
+// document.write(lastname);
+// document.write("<br><br>");
+// document.write(typeof lastname);
 
-document.write("<br><br>");
-document.write(b);
-document.write("<br><br>");
-document.write(typeof b);
+// document.write("<br><br>");
+// document.write(cnic);
+// document.write("<br><br>");
+// document.write(typeof cnic);
 
-document.write("<br><br>");
-document.write(fullname);
+// document.write("<br><br>");
+// document.write(a);
+// document.write("<br><br>");
+// document.write(typeof a);
+
+// document.write("<br><br>");
+// document.write(b);
+// document.write("<br><br>");
+// document.write(typeof b);
+
+// document.write("<br><br>");
+// document.write(fullname);
