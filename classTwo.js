34000b12768a3c563b4d5a5c9db321c3ad35b0f5
Nasir-Hussain786 <starbro646@gmail.com>
2025-12-31 22:06:37 +0500

    practice variables

---
+++ classTwo.js
@@ -0,0 +1,40 @@
+let firstName = "Nasir"; 
+    firstName = "Hussain Nasir";
+    firstName = 'Nasir Hussain';
+    firstName = "Nasir Hussain Khanzada";
+    firstName = "Nasir Khanzada";
+    firstName = 'Nasir khanzada Rajput';
+    firstName = 'Khanzada Nasir Rajput';
+    firstName = 'khanzada Nasir Hussain Rajput';
+
+let _lastName = 'Hussain';
+    _lastName = "khanzada";
+    _lastName = 'Rajput';
+    _lastName = 'khanzada Rajput';
+
+let $age = 17;
+    $age = 18;
+    $age = 21;
+    $age = 22;
+    $age = 30;
+
+let myhome;
+let yourhome;
+
+let a = null ;
+
+let city = "karachi";
+    city = "Hyderabad";
+    city = "Nawabshah";
+    city = "Naushahro feroze";
+    city = "Sukkur";
+    city = 'Multan';
+    city = 'Lahore';
+
+const schoolName = "Star Grammer";
+const dateOfBirth = 30-9-2009;
+const websiteName = "google";
+const country = "Pakistan";
+
+let myvalues = [99, "computer" , true , null , undefined];
+const subjects = ["English" , "Math" , "chemistry" , "Physics" , "Science"];
