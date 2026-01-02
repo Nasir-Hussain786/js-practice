396074b1ca38c90731b937555406cf58cc16617a
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-02 11:11:18 +0500

    practice addition operator

---
+++ operator-practice.js
@@ -0,0 +1,51 @@
+// *** ---- Addition Operator ---- ***
+
+let ab = 44;
+let ac = 20;
+let ad = ab + ac;
+console.log(ad);
+
+let ba = 555;
+let ca = 433;
+let da = ba + ca;
+console.log(da);
+
+let _ab = 90;
+let _ac = 99;
+let _ad = _ab + _ac;
+console.log (_ad);
+
+let $ad = 320;
+let $bd = 40;
+let $daa = $ad + $bd;
+console.log ($daa);
+
+let abcd = 100;
+let abe = 40;
+let adge = abcd + abe;
+console.log(adge);
+
+let ged = 430;
+let daef = 70;
+let gede = ged + daef;
+console.log(gede);
+
+let eate = 999;
+let trea = 1;
+let jii = eate + trea;
+console.log(jii);
+
+let liie = 84;
+let lie = 6;
+let liee = liie + lie;
+console.log(liee);
+
+let kli = 120;
+let lli = 40;
+let klio = kli + lli;
+console.log(klio);
+
+
+
+
+
