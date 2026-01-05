b6db9becd7d636bd1319fff3b70bf06c7fa8c86d
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-05 15:50:26 +0500

    if & else practice

---
+++ condition.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = 10;
+
+if(a == b){
+    console.log("A equal to b");
+};
+
+
+let age = 18;
+
+if(age >= 18){
+    console.log("You are Adult");
+}else{
+    console.log("You are minor");
+};
+
+let marks = 60;
+
+if(marks >= 45 && marks <= 60){
+    document.write("Your Grade is <b> C </b>");
+};
