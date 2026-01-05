1183db9aec330caf948ade82df98ca092360593e
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-05 18:32:28 +0500

    practice logical and operator

--- condition.js
+++ condition.js
@@ -14,8 +14,34 @@ if(age >= 18){
     console.log("You are minor");
 };
 
-let marks = 60;
+
+let marks = 49;
 
 if(marks >= 45 && marks <= 60){
     document.write("Your Grade is <b> C </b>");
+}else if( marks >= 61 && marks <= 75){
+    document.write("Your Grade is <b>B</b>");
+}else if(marks >= 76 && marks <= 100){
+    document.write("Your Grade is <b>A</b>");
+}else if(marks >= 33 && marks <= 44 ){
+    document.write("<b> Fail </b>");
+}else{
+    document.write("<b> Invalid Number </b>");
+};
+
+
+document.write("<br><br>")
+
+// Traffic Singal
+
+let signal = "Red";
+
+if(signal === "Red"){
+    console.log("Stop");
+}else if(signal === "Yellow"){
+    console.log("Ready");
+}else if(signal === "Green"){
+    console.log("Go");
+}else{
+    console.log("Invalid Signal");
 };
