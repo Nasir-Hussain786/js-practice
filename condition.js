d91656912b85a7598d38ccd3931613b8940859d8
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-06 15:04:20 +0500

    comment all previous code

--- condition.js
+++ condition.js
@@ -1,47 +1,65 @@
-let a = 10;
-let b = 10;
-
-if(a == b){
-    console.log("A equal to b");
-};
 
 
-let age = 18;
-
-if(age >= 18){
-    console.log("You are Adult");
-}else{
-    console.log("You are minor");
-};
 
 
-let marks = 49;
-
-if(marks >= 45 && marks <= 60){
-    document.write("Your Grade is <b> C </b>");
-}else if( marks >= 61 && marks <= 75){
-    document.write("Your Grade is <b>B</b>");
-}else if(marks >= 76 && marks <= 100){
-    document.write("Your Grade is <b>A</b>");
-}else if(marks >= 33 && marks <= 44 ){
-    document.write("<b> Fail </b>");
-}else{
-    document.write("<b> Invalid Number </b>");
-};
 
 
-document.write("<br><br>")
 
-// Traffic Singal
 
-let signal = "Red";
 
-if(signal === "Red"){
-    console.log("Stop");
-}else if(signal === "Yellow"){
-    console.log("Ready");
-}else if(signal === "Green"){
-    console.log("Go");
-}else{
-    console.log("Invalid Signal");
-};
+
+
+
+
+
+
+
+
+
+// let a = 10;
+// let b = 10;
+
+// if(a == b){
+//     console.log("A equal to b");
+// };
+
+
+// let age = 18;
+
+// if(age >= 18){
+//     console.log("You are Adult");
+// }else{
+//     console.log("You are minor");
+// };
+
+
+// let marks = 49;
+
+// if(marks >= 45 && marks <= 60){
+//     document.write("Your Grade is <b> C </b>");
+// }else if( marks >= 61 && marks <= 75){
+//     document.write("Your Grade is <b>B</b>");
+// }else if(marks >= 76 && marks <= 100){
+//     document.write("Your Grade is <b>A</b>");
+// }else if(marks >= 33 && marks <= 44 ){
+//     document.write("<b> Fail </b>");
+// }else{
+//     document.write("<b> Invalid Number </b>");
+// };
+
+
+// document.write("<br><br>")
+
+// // Traffic Singal
+
+// let signal = "Red";
+
+// if(signal === "Red"){
+//     console.log("Stop");
+// }else if(signal === "Yellow"){
+//     console.log("Ready");
+// }else if(signal === "Green"){
+//     console.log("Go");
+// }else{
+//     console.log("Invalid Signal");
+// };
