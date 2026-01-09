d18cc849071a854375f8920e50146e8266f7e5eb
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-09 20:33:15 +0500

    practice string length and index

---
+++ loop-practice.js
@@ -0,0 +1,18 @@
+let text = "Learn Javascript";
+console.log(text.length ,text[0], text[15]);
+
+let months = ["jan","feb",'mar','apr','may','jun'];
+console.log(months.length , months[0], months[4]);
+
+let number = [20, 30, 33, 55, 99];
+console.log("Length of number is", number.length ,"third number of index is ", number[3] );
+
+let mixed = ['Bilal', 22, true, undefined, 4.4];
+console.log(mixed.length ,mixed[0], mixed[2] , mixed[4]);
+
+
+//****---- Loop practice ----****
+
+for(let i = 1; i <= 10 ; i++){
+    console.log("Nasir khanzada" , i);
+}
