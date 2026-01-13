5fb688bf48313df4ad58faedaf9a599ca3fdf833
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-13 18:57:19 +0500

    loop break & loop continue practice

---
+++ while-loop.js
@@ -0,0 +1,32 @@
+// **** ---- while loop practice ----****
+
+let i = 0; 
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+// ****---- For Loop Practice ----****
+
+const fruits = ["banana","mango","orange","apple"];
+for(let i = 0; i < fruits.length; i++){
+    console.log(i);
+}
+
+// ****---- Loop Break ----****
+for(let i = 0; i <= 20; i++){
+    if(i == 13){
+        break;
+    }else{
+        console.log(i);
+    }
+}
+
+// ****---- Loop continue ----****
+for(let i = 0; i < 5; i++){
+    if(i == 3){
+        continue;
+    }else{
+        console.log(i);
+    }
+}
