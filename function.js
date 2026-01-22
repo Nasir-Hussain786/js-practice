29f077f983ec0365b69c89718f14d0d831ff04ff
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-22 22:20:04 +0500

    learn and practice javascript airthmetic functions

--- function.js
+++ function.js
@@ -1,6 +1,31 @@
+// **** ---- addition function ----**** 
 function add(a,b){
     return a + b;
 }
 
 let result = add(9,9);
 console.log(result);
+
+// **** ---- Substraction function ----**** 
+function substraction(a,b){
+    return a - b;
+}
+
+let result1 = substraction(20,7);
+console.log(result1)
+
+// **** ---- Multiplication function ----****
+function mul(a,b){
+    return a * b;
+} 
+
+let result3 = mul(8,7);
+console.log(result3);
+
+// **** ---- Division function ----**** 
+function divide(a,b){
+    return a/b;
+}
+
+let result2 = divide(42,7);
+console.log(result2);
