35cf571115c08d79691623f29d9a7aabb5458549
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-26 19:39:08 +0500

    practice onclick

--- function.js
+++ function.js
@@ -29,3 +29,47 @@ function divide(a,b){
 
 let result2 = divide(42,7);
 console.log(result2);
+
+
+// square
+function square(num){
+    return num * num;
+}
+
+let numberSquare = square(9,9);
+console.log(numberSquare);
+
+// string
+function capitalWords (firstName){
+    return firstName.toUpperCase();
+}
+console.log(capitalWords("nasir hussain"));
+
+// fullName
+function fullName(firstName,lastName){
+    return firstName + " " + lastName;
+}
+
+console.log(fullName("Hasnain","Anwar"));
+
+function greet(){
+    alert("Assalam O Alaikum"); 
+}
+
+function add(){
+    let a = 10;
+    let b = 50;
+    alert("10 + 50 = " + " " + (a + b));
+}
+
+function sub(){
+    let a = 90;
+    let b = 50;
+    alert("90 - 50 = " + " " +  (a - b));
+}
+
+function mul(){
+    let a = 6;
+    let b = 7;
+    alert("6 * 7 = " + " " + (a * b));
+}
