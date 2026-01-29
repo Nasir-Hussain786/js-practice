d49f175413e57f766c1b3f53d1e933297d1bf42d
Nasir-Hussain786 <starbro646@gmail.com>
2026-01-29 20:36:22 +0500

    js object practice

---
+++ object.js
@@ -0,0 +1,47 @@
+const studentData = [
+    {firstName : "Nasir" , lastName : "Hussain"},
+    {subject : "Math" , score : 100},
+    {subject : "English" , score : 100},
+    allSubjects = ["Math","English","Science","Chemistry","Physics"],
+];
+
+const school = {
+    name :"Ali",
+    class : 10,
+    age : 15,
+    subjects : ["Biology","Urdu","Math"]
+};
+
+
+const student = {
+    name : "Ali",
+    age : 20,
+    subjects : ["Math","Urdu","English"],
+};
+
+
+const car = {
+    brand : "Toyota",
+    model : 2024,
+    color : "Black",
+};
+
+const mobile = {
+    company : "Apple",
+    price : 120000,
+    color : "Black",
+    feature : ["Camera","Bluetooth"]
+};
+
+const user = {
+    username : "starbro",
+    password : 3234324,
+    hobbies : ["Coding","gaming"]
+};
+
+const book = {
+    title : "JavaScript Basics",
+    pages : 200,
+    author : "Brendan Eich",
+    age : 64
+};
